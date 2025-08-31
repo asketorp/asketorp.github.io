fae18ec09bb61135e629704f616266fd5059524e
Johan <jernlas@gmail.com>
2025-08-31 10:15:30 +0200

    Use built in filename extraction

--- build_file_list.sh
+++ build_file_list.sh
@@ -5,17 +5,17 @@ echo "" > dynfile.js
 
 # Dikningsföretag
 echo "var dikning_files = [" >> dynfile.js
-find res/dynamic/dikningsforetag/ -type f | sort | xargs filename | sed "s/^\(.*\)$/{file: '\1'},/" >> dynfile.js
+find res/dynamic/dikningsforetag/ -type f | sort | xargs basename -a | sed "s/^\(.*\)$/{file: '\1'},/" >> dynfile.js
 echo "];" >> dynfile.js
 
 # Vägsamfällighet
 echo "var vag_files = [" >> dynfile.js
-find res/dynamic/vagsamfallighet/ -type f | sort | xargs filename | sed "s/^\(.*\)$/{file: '\1'},/" >> dynfile.js
+find res/dynamic/vagsamfallighet/ -type f | sort | xargs basename -a | sed "s/^\(.*\)$/{file: '\1'},/" >> dynfile.js
 echo "];" >> dynfile.js
 
 # Byalag
 echo "var by_files = [" >> dynfile.js
-find res/dynamic/byalag/ -type f | sort | xargs filename | sed "s/^\(.*\)$/{file: '\1'},/" >> dynfile.js
+find res/dynamic/byalag/ -type f | sort | xargs basename -a | sed "s/^\(.*\)$/{file: '\1'},/" >> dynfile.js
 echo "];" >> dynfile.js
 
 
